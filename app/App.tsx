7d82ddd11459af1ccd6b1e716baf1cba31a6c241
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2023-02-11 19:53:25 -0600

    Setup basic UI elements

--- app/App.tsx
+++ app/App.tsx
@@ -1,117 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {View, Button, ScrollView, Text, Alert, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const submitMood = () => {
+  Alert.alert('hi');
+};
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="see you space bebop">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits. Howdy~!
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next boi yeehawwww!!!:
-          </Section>
-          <LearnMoreLinks />
+    <ScrollView style={styles}>
+      <View>
+        <Text>Profile</Text>
+        <Text>Sunny</Text>
+        <Text>Currently</Text>
+        <Text style={styles.tempText}>58</Text>
+        <Text>Feels Like</Text>
+        <Text style={styles.tempText}>47</Text>
+      </View>
+      <View>
+        <Text>How does the weather make you feel?</Text>
+        <View style={styles.buttonView}>
+          <Button title="Bad" onPress={submitMood} style={styles.flexButton} />
+          <Button title="Meh" onPress={submitMood} />
+          <Button title="Neutral" onPress={submitMood} />
+          <Button title="Good" onPress={submitMood} />
+          <Button title="Great" onPress={submitMood} />
         </View>
-      </ScrollView>
-    </SafeAreaView>
+      </View>
+      <View>
+        <Text>What's the style?</Text>
+        <View style={styles.buttonView}>
+          <Button title="Beach Outfit" onPress={submitMood} />
+          <Button title="Warm" onPress={submitMood} />
+          <Button title="Whatever" onPress={submitMood} />
+          <Button title="Sweater Weather" onPress={submitMood} />
+          <Button title="Bundled Up" onPress={submitMood} />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  baseText: {
+    color: 'black',
+    backgroundColor: 'white',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  buttonView: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: 100,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  flexButton: {
+    flexGrow: 1,
   },
-  highlight: {
-    fontWeight: '700',
+  tempText: {
+    fontSize: 36,
   },
 });
 
