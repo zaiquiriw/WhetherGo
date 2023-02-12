14acbe1b171bc8721eef00b5e442d1b142791d9e
Zachary Canoot <zaiquiriw@gmail.com>
2023-02-12 01:41:03 -0600

    idk

--- server/routes/record.js
+++ server/routes/record.js
@@ -14,27 +14,21 @@ function createCrossingRequest(location, date1, date2) {
 	return baseURL + location + "/" + date1 + "/" + date2 + "?key=" + apiKey
 }
 
-<<<<<<< HEAD
-router.use((req, res, next) => {
-	console.log('Time: ', new Date().toISOString())
-	next()
-=======
 const crossingBaseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 
 // return weather data from 
 router.get('/api/crossing/', function (req, res) {
 	let location = "seattle%20WA"
-	let fullUrl = crossingBaseURL + location + "/today?unitGroup=us&key="+ process.env.CROSS_KEY + "&contentType=json&elements=temp,tempmin,tempmax,icon,datetime,feelslike&include=current"
-	
+	let fullUrl = crossingBaseURL + location + "/today?unitGroup=us&key=" + process.env.CROSS_KEY + "&contentType=json&elements=temp,tempmin,tempmax,icon,datetime,feelslike&include=current"
+
 	fetch(fullUrl)
 		.then((res) => {
 			if (res.status >= 400) {
-					throw new Error("Bad response from server");
+				throw new Error("Bad response from server");
 			}
 			return res.json();
 		})
 		.then((data) => res.send(data));
->>>>>>> 47716d4c5ca58956ed7c1085c352770d69568d57
 })
 
 module.exports = router;
