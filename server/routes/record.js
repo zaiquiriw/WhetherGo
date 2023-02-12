47716d4c5ca58956ed7c1085c352770d69568d57
Liz Wigglesworth <liz.wigglesworth@gmail.com>
2023-02-12 01:30:58 -0600

    Pull weather data from API

--- server/routes/record.js
+++ server/routes/record.js
@@ -1,6 +1,5 @@
-const { application } = require("express")
 const express = require("express")
-
+require('isomorphic-fetch');
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
@@ -15,8 +14,21 @@ function createCrossingRequest(location, date1, date2) {
 	return baseURL + location + "/" + date1 + "/" + date2 + "?key=" + apiKey
 }
 
+const crossingBaseURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+
+// return weather data from 
 router.get('/api/crossing/', function (req, res) {
-	res.send(req.query.abc)
+	let location = "seattle%20WA"
+	let fullUrl = crossingBaseURL + location + "/today?unitGroup=us&key="+ process.env.CROSS_KEY + "&contentType=json&elements=temp,tempmin,tempmax,icon,datetime,feelslike&include=current"
+	
+	fetch(fullUrl)
+		.then((res) => {
+			if (res.status >= 400) {
+					throw new Error("Bad response from server");
+			}
+			return res.json();
+		})
+		.then((data) => res.send(data));
 })
 
 module.exports = router;
